ccb9481a3f7f32090a65ca4097cb5a54c4d30761
Evy Svensson <evymail87@gmail.com>
2020-05-08 09:51:20 +0200

    Fungerar att hämta data från soap-api dock ej att isolera

--- Script1.js
+++ Script1.js
@@ -10,14 +10,14 @@ function getCountries() {
 
     //Input till parametern
     //var k = "SE";
-    //const utest = 'http://www.oorsprong.org/websamples.countryinfo';
+    const utest = 'http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL';
    // console.log(document.getElementById("countrycode").innerHTML = k);
     //console.log(document.getElementsByTagName("div"));
 
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open("GET", 'http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL', true);
-
+ 
 
     var soapmessage =
         `<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:web="http://www.oorsprong.org/websamples.countryinfo">
@@ -28,43 +28,37 @@ function getCountries() {
       </web:CapitalCity>
    </soap:Body>
 </soap:Envelope>`;
-    
+    xmlhttp.setRequestHeader("Content-Type", "text/xml");
+    xmlhttp.send(soapmessage);
     
 
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4) {
             if (xmlhttp.status == 200) {
                 // prints the response to the console
+           
+              
+                  
                 console.log(xmlhttp.responseXML);
             }
+                
+            }
         }
-    }
+    
+    
 
-    xmlhttp.setRequestHeader("Content-Type", "text/xml");
-    xmlhttp.send(soapmessage)
       //  .then(data => console.log(data))
     //console.log(XMLDocument.getElementByTagName("m:CapitalCityResult"));
-   // console.log(document.getElementById("innertest"));
+
 }
 getCountries();
 
 
-function soapRequest() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('POST', '<enter url to endpoint here>', true);
 
-    // Build SOAP request
-  
 
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4) {
-            if (xmlhttp.status == 200) {
-                // prints the response to the console
-                console.log(xmlhttp.response)
-            }
-        }
-    }
-    // Send POST request
-    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-    xmlhttp.send(soapmessage)
-}
+
+
+
+
+
+
